91ee56ab873c227d8e76162960cdf774b1fc7dfc
Magdalena Lonc <magdalena.lonc@gmail.com>
2024-03-19 00:00:14 +0100

    Add custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Shopping App',
+      theme: ThemeData(
+        fontFamily: 'Lato',
+      ),
       home: HomePage(),
     );
   }
